36e9900cbdbcab40faabc5e1ebefc643a331375c
samralck <145826225+samralck@users.noreply.github.com>
2026-06-24 16:51:10 +1000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC9d92RceFiW1C7YnirORU9ECsC4As7Tv4",
-  authDomain: "lennys-training.firebaseapp.com",
-  projectId: "lennys-training",
-  storageBucket: "lennys-training.firebasestorage.app",
-  messagingSenderId: "841079792496",
-  appId: "1:841079792496:web:d6946153382ddbaf1625b5",
-  measurementId: "G-ZSNLHV2RFY"
+  apiKey: "AIzaSyClgDCnLHRDZKvsb5cT0c72cnNphoppIA0",
+  authDomain: "lennysrecall.firebaseapp.com",
+  projectId: "lennysrecall",
+  storageBucket: "lennysrecall.firebasestorage.app",
+  messagingSenderId: "291680660028",
+  appId: "1:291680660028:web:e4b519f1825fe965d97139"
 };
